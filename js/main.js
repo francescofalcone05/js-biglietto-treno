a678cea163a204f4921216aa7ce58afdef345f40
francescofalcone05 <ciccio_97@live.it>
2024-04-17 17:13:15 +0300

    calc-2

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@ const userAge = prompt ("Scrivi la tua eta`");
 const userkm = prompt ("Scrivi i kilometri che dovrai percorrere");
 
 let priceForKm = 0.21
-let ticketPrice = priceForKm * userkm 
+//let ticketPrice = "" 
+let ticketPrice = priceForKm * userkm
+let juniorDiscount = ticketPrice-ticketPrice*20/100
+let seniorDiscount = ticketPrice-ticketPrice*40/100
+
 
 
 if ((userAge > 18) || (userAge < 65)) {
@@ -13,7 +17,10 @@ if ((userAge > 18) || (userAge < 65)) {
     
 } else if (userAge < 18) {
 
-    let ticketPrice = ticketPrice - ticketPrice*20/100
+    let ticketPrice = juniorDiscount
+} else if (userAge > 65) {
+
+    let ticketPrice = seniorDiscount
 }
 
 
