b6480d1c5fee3eaf382dfe7c9c800f047293dfc4
francescofalcone05 <ciccio_97@live.it>
2024-04-17 16:39:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const userName = prompt ("Scrivi il tuo nome");
+const userSurname = prompt ("Scrivi il tuo cognome");
+const userAge = prompt ("Scrivi la tua eta`");
+const userkm = prompt ("Scrivi i kilometri che dovrai percorrere");
+
+let priceForKm = 0.21
+let ticketPrice = priceForKm * userkm 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("name").innerHTML = userName
+document.getElementById("surname").innerHTML = userSurname
+document.getElementById("age").innerHTML = userAge
+document.getElementById("km-t").innerHTML = userkm
+document.getElementById("price").innerHTML = ticketPrice
+
+
